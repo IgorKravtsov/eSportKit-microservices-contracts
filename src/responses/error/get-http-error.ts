04aa316500f149a2fd546b00534fbee16755d6f8
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-08-09 18:37:16 +0300

    changes in error-response - 1.0.52

--- src/responses/error/get-http-error.ts
+++ src/responses/error/get-http-error.ts
@@ -2,8 +2,8 @@ import { ErrorCodes } from './enums';
 import { MicroserviceException } from './microservice-error';
 import { IErrorResponse, ILogger } from './interfaces';
 
-export const getHttpError = (err: Error, logger: ILogger): IErrorResponse => {
-  if (err instanceof MicroserviceException) {
+export const getHttpError = (err: MicroserviceException, logger: ILogger): IErrorResponse => {
+  if (err.devMessage) {
     logger.debug ? logger.debug(err.devMessage) : logger.error(err.devMessage);
     return { body: { message: err.message, statusCode: err.code }, code: err.code };
   } else {
