64897881a33c2f0d4f318baaed1cba702391add1
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-08-09 18:58:42 +0300

    error response changes - 1.0.53

--- src/responses/error/microservice-error.ts
+++ src/responses/error/microservice-error.ts
@@ -1,12 +1,36 @@
 import { ErrorCodes } from './enums';
+import { IErrorResponse, ILogger } from './interfaces';
 
 export class MicroserviceException extends Error {
-  devMessage: string;
-  code: ErrorCodes;
+  public devMessage: string;
+  public code: ErrorCodes;
+  public message: string;
+  public data?: any;
+  public service?: string;
+  public host?: string;
+  public type?: any;
 
-  constructor(message: string, devMessage: string, code: ErrorCodes) {
+  constructor(message: string, type: any, code: ErrorCodes, data?: any, service?: string, host?: string) {
     super(message);
-    this.devMessage = devMessage;
     this.code = code;
+    this.type = type;
+    this.data = data;
+    this.service = service;
+    this.host = host;
+
+    this.devMessage = `[${host}]/[${service}]: ${message} (code: ${code})`;
+  }
+
+  getHttpError(err: MicroserviceException, logger?: ILogger): IErrorResponse {
+    if (err.devMessage) {
+      logger?.debug ? logger?.debug(err.devMessage) : logger?.error(err.devMessage);
+      return { body: { message: err.message, statusCode: err.code }, code: err.code };
+    } else {
+      logger?.error(err.message);
+      return {
+        body: { message: 'Something went wrong', statusCode: ErrorCodes.InternalError },
+        code: ErrorCodes.InternalError
+      };
+    }
   }
 }
