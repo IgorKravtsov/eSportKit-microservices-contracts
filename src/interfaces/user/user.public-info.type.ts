075ae6e621425ca693f89c9ea91fca81816c3ecd
Igor Kravtsov <kravtsov.igor.karate@gmail.com>
2022-08-15 16:45:44 +0300

    updated IUserPublicInfo - 1.1.0

--- src/interfaces/user/user.public-info.type.ts
+++ src/interfaces/user/user.public-info.type.ts
@@ -1,3 +1,3 @@
 import { IUser } from './user.interface';
 
-export type IUserPublicInfo = Pick<IUser, '_id' | 'email' | 'name' | 'lastName' | 'middleName' | 'level' | 'role'>;
+export type IUserPublicInfo = Pick<IUser, '_id' | 'email' | 'name' | 'lastName' | 'middleName' | 'bio'>;
